c69e264d7b600c235e7168201675746c4a2f6e30
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-08-20 10:25:37 +0300

    fetch movies

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 
 void main() => runApp(MoviesApp());
 
+final apiKey = "0c512e728ca1c12431b97e545eb09306";
+
 class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,8 +21,39 @@ class MoviesListing extends StatefulWidget {
 }
 
 class _MoviesListingState extends State<MoviesListing> {
+  //Variable to hold movies information
+  var movies;
+
+  //Method to make http requests
+  static dynamic getJson() async {
+    //URL to fetch movies information
+    final apiEndPoint =
+        "http://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc";
+    final apiResponse = await http.get(apiEndPoint);
+    return apiResponse;
+  }
+
+  //Method to fetch movies from network
+  fetchMovies() async {
+    var data = await getJson();
+
+    setState(() {
+      movies = data;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    //Fetch movies
+    fetchMovies();
+
+    return Scaffold(
+      //SingleChildScrollView to provide scrolling for flexible data rendering
+      body: SingleChildScrollView(
+        child: movies != null
+            ? Text("API response\n $movies")
+            : Text("Loading ..."),
+      ),
+    );
   }
 }
