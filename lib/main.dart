5e1ab626c76dc2279aa0c7d910edeca9ffc740e7
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-08-20 12:30:37 +0300

    Rebuilded response structure for performance with MovieModel class

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,13 @@ class MoviesApp extends StatelessWidget {
 }
 
 class MoviesProvider {
-  static Future<Map> getJson() async {
-    final apiKey = "0c512e728ca1c12431b97e545eb09306";
+  static final String imagePathPrefix = 'https://image.tmdb.org/t/p/w500/';
+  static final apiKey = "0c512e728ca1c12431b97e545eb09306";
 
+  static Future<Map> getJson() async {
     final apiEndPoint =
         "http://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc";
-
     final apiResponse = await http.get(apiEndPoint);
-
     //Parsing to JSON using dart:convert
     return json.decode(apiResponse.body);
   }
@@ -34,36 +33,67 @@ class MoviesListing extends StatefulWidget {
 }
 
 class _MoviesListingState extends State<MoviesListing> {
-  //Variable to hold movies information
-  var movies;
-
+  List<MovieModel> movies = List<MovieModel>();
   //Method to fetch movies from network
   fetchMovies() async {
     var data = await MoviesProvider.getJson();
 
     setState(() {
-      movies = data['results'];
+      List<dynamic> results = data['results'];
+      results.forEach((element) {
+        movies.add(MovieModel.fromJson(element));
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    //Fetch movies
     fetchMovies();
 
     return Scaffold(
-        //Rendering movies in ListView
-        body: ListView.builder(
-      itemCount: movies == null ? 0 : movies.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          //Adding padding around the list row
-          padding: const EdgeInsets.all(8.0),
-
-          //Displaying title of the movie only for now
-          child: Text(movies[index]["title"]),
-        );
-      },
-    ));
+      body: ListView.builder(
+        itemCount: movies == null ? 0 : movies.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(movies[index].title),
+          );
+        },
+      ),
+    );
   }
+}
+
+//JSON response is converted into MovieModel object
+class MovieModel {
+  final int id;
+  final num popularity;
+  final int vote_count;
+  final bool video;
+  final String poster_path;
+  final String backdrop_path;
+  final bool adult;
+  final String original_language;
+  final String original_title;
+  final List<dynamic> genre_ids;
+  final String title;
+  final num vote_average;
+  final String overview;
+  final String release_date;
+
+  MovieModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        popularity = json['popularity'],
+        vote_count = json['vote_count'],
+        video = json['video'],
+        poster_path = json['poster_path'],
+        adult = json['adult'],
+        original_language = json['original_language'],
+        original_title = json['original_title'],
+        genre_ids = json['genre_ids'],
+        title = json['title'],
+        vote_average = json['vote_average'],
+        overview = json['overview'],
+        release_date = json['release_date'],
+        backdrop_path = json['backdrop_path'];
 }
